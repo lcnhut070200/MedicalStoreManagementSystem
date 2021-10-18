631e80588ebfdf5a08206fee4077df6a7f69c672
lcnhut070200 <lcnhut070200@gmail.com>
2021-10-18 23:34:55 +0700

    style: Theme setup | Login page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./pages/Login";
 
-ReactDom.render(<h1>Hello</h1>, document.getElementById("root"));
+ReactDom.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Login}></Route>
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
